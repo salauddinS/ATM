f2245b7ac25f32c033e88cef00592e4870617f0c
Salauddin Shaikh <salauddin8097@gmail.com>
2017-02-19 12:56:07 +0530

    login

--- src/client/app/features/login/login.controller.js
+++ src/client/app/features/login/login.controller.js
@@ -4,12 +4,14 @@
     angular.module('ATMapp').controller('LoginController', LoginController);
 
     LoginController.$inject = ['authService', '$location', '$rootScope'];
-    
+
     /* @ngInject */
     function LoginController(authService, $location, $rootScope) {
         /* jshint validthis: true */
         var vm = this;
+        vm.user={};
         vm.login = login;
+        
         activate();
 
         function activate() {
@@ -17,7 +19,11 @@
                 $location.path('/');
             }
         }
+
+
         function login() {
+            vm.user.userName = vm.box1 + vm.box2 + vm.box3 + vm.box4;
+            console.log(vm);
             authService.login(vm.user).then(function (response) {
                 localStorage.setItem('accessToken', response.data.token);
                 $rootScope.$broadcast('loggedInSuccessfull')
@@ -26,5 +32,6 @@
                 console.log(err)
             });
         }
+
     }
 })();
